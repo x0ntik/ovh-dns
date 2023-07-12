a7b967aa01498cf9b9f1ef50294791066eca6dad
xontik <xontik@gmail.com>
2023-07-12 15:37:14 +0200

    add multi zone compatibility

--- Dockerfile
+++ Dockerfile
@@ -9,6 +9,7 @@ COPY package*.json ./
 
 RUN npm install
 
-COPY src src
+COPY src ./
+copy zones.json ./
 
 ENTRYPOINT [ "node", "src/index.js" ]
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+require('dotenv').config();
 
 const ovh = require('ovh')({
   endpoint: 'ovh-eu',
@@ -9,44 +9,67 @@ const ovh = require('ovh')({
 const publicIp = require('public-ip');
 const cron = require('node-cron');
 
-const zone = process.env.ZONE_DNS;
+const zones = require('./zones.json');
 
-const crontInterval = process.env.CRON_SCHEDULE
-const subDomains = process.env.SUB_DOMAINS.split(',')
+const crontInterval = process.env.CRON_SCHEDULE;
 async function getRecord(zone, params) {
-  const records = await ovh.requestPromised('GET', `/domain/zone/${zone}/record`, params);
+  const records = await ovh.requestPromised(
+    'GET',
+    encodeURI(`/domain/zone/${zone}/record`),
+    params
+  );
 
   if (records.length !== 1) {
-    throw new Error('record not uniq or no record');
+    throw new Error(
+      `record not uniq or no record ${zone} dom ${params.subDomain}`
+    );
   }
-  return await ovh.requestPromised('GET', `/domain/zone/${zone}/record/${records[0]}`, {});
+  return await ovh.requestPromised(
+    'GET',
+    encodeURI(`/domain/zone/${zone}/record/${records[0]}`),
+    {}
+  );
 }
 
-cron.schedule(`${crontInterval}`, async () => {
-  for (const subdomain of subDomains) {
-    try {
-      const params = {
-        fieldType: process.env.FIELD_TYPE,
-        subDomain: subdomain
-      };
-      const [record, ip] = await Promise.all([getRecord(zone, params), publicIp.v4()]);
-      console.log(`Subdomain : -->${subdomain}<--`)
-      if (record.target === ip) {
-        console.log(`Already good ip do nothing`)
-        continue;
+for (const zone of zones) {
+  console.log(`Start for ${zone.name}`);
+  cron
+    .schedule(`${crontInterval}`, async () => {
+      for (const subdomain of zone.subDomains) {
+        console.log(`Start for ${subdomain} of ${zone.name}`);
+        try {
+          const params = {
+            fieldType: process.env.FIELD_TYPE,
+            subDomain: subdomain,
+          };
+          const [record, ip] = await Promise.all([
+            getRecord(zone.name, params),
+            publicIp.v4(),
+          ]);
+          console.log(`Subdomain : -->${subdomain}<--`);
+          if (record.target === ip) {
+            console.log(`Already good ip do nothing`);
+            continue;
+          }
+
+          console.log(`Changing ip from ${record.target} to ${ip}`);
+
+          const putRequest = await ovh.requestPromised(
+            'PUT',
+            encodeURI(`/domain/zone/${zone.name}/record/${record.id}`),
+            {
+              target: ip,
+            }
+          );
+
+          const refresh = await ovh.requestPromised(
+            'POST',
+            encodeURI(`/domain/zone/${zone.name}/refresh`)
+          );
+        } catch (e) {
+          console.error(e);
+        }
       }
-
-      console.log(`Changing ip from ${record.target} to ${ip}`);
-
-      const putRequest = await ovh.requestPromised('PUT', `/domain/zone/${zone}/record/${record.id}`, {
-        target: ip
-      });
-
-      const refresh = await ovh.requestPromised('POST', `/domain/zone/${zone}/refresh`);
-
-    } catch (e) {
-      console.error(e)
-    }
-  }
-}).start()
-
+    })
+    .start();
+}
